cbc508049e58439aa2fb43a9146de7bac132cb9f
jarell Boone <jarellboone4@gmail.com>
2021-03-10 23:07:56 -0500

    had to restructure file system and somehow my server runs but shows an error will fix in the morning

---
+++ config/models/burger.js
@@ -0,0 +1,20 @@
+const orm = require('../orm');
+
+const burger = {
+  all(cb) {
+    orm.selectAll('burgers', (res) => cb(res));
+  },
+  // The variables cols and vals are arrays.
+  create(cols, vals, cb) {
+    orm.insertOne('burgers', cols, vals, (res) => cb(res));
+  },
+  update(objColVals, condition, cb) {
+    orm.updateOne('burgers', objColVals, condition, (res) => cb(res));
+  },
+  delete(condition, cb) {
+    orm.deleteOne('burgers', condition, (res) => cb(res));
+  },
+};
+
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;
